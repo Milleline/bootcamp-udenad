d78cd9a5b436d33f9462cf6748a31473bd817c3d
Milleline <U67850@rts-365.dk>
2021-08-19 12:34:49 +0200

    Add solution 02b

--- eks_02c_funktioner/js/index.js
+++ eks_02c_funktioner/js/index.js
@@ -1,6 +1,16 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
+
+
+	
+	// 02c
+	//1. Kopier koden fra opgave fra 02b.
+	//2. Istedet for at kalde din console.log i funktionen, skal der nu stå: KODE : return "Hej med dig, " + navn;
+	//3. Der hvor du kalder din funktion skal du nu oprette en variabel til at "tage imod" svaret. : KODE : var hilsenFraOle = hilsen("Ole");
+	//4. Udskriv variablen i konsollen.
+
+
 	// skriv din kode her...
 
 }); // Afslutter: DOMContentLoaded
