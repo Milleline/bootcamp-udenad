1eb8cf64e485bb82cbe4a5c09895e77bc7c7b09b
Milleline <U67850@rts-365.dk>
2021-08-19 10:41:19 +0200

    Add solution 02a

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,6 +1,20 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
+	//1.Opret en funktion som hedder Hils, lad funktionen skrive "Hej med dig" i konsollen.
+	//2.Har det betydning om du kalader funktionen før eller efter den er oprettet? 
+	//SVAR: Nej det har ingen betydning.
+	
 	// skriv din kode her...
 
+	hils();
+
+	function hils(){
+		
+		console.log("Hej med dig")
+	}
+
+	hils();
+
+	
 }); // Afslutter: DOMContentLoaded
